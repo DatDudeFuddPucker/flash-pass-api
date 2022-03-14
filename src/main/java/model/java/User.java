f52d16eb627a7b7d49e6c5606ce47e3d0bd782a7
Nivedh <1ollakal@gmail.com>
2022-03-14 18:48:51 -0400

    Basic Registration Cleanup

--- src/main/java/model/java/User.java
+++ src/main/java/model/java/User.java
@@ -4,6 +4,30 @@ public class User {
     public String username;
     public String password;
 
+    // no argument constructor
+    public User() {
+
+    }
+
+    // This is a 2 argument constructor
+    public User(String u, String p){
+        this.username = u;
+        this.password = p;
+    }
+
+    // Example of use of Polymorphism: Overloading
+    public void overloadingExample(){
+        System.out.println("Hello");
+    }
+
+    public void overloadingExample(String input){
+        System.out.println(input);
+    }
+
+    public void overloadingExample(String name, String location){
+
+    }
+
     @Override
     public String toString() {
         return "User{" +
